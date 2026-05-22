4650a1a188d2ef4a7453d36e16edbac3df1e7027
Julio Suarez <114452773+juliorbk@users.noreply.github.com>
2026-05-21 22:06:54 -0400

    remove api rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -7,14 +7,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-      },
-    ]
-  },
+
 }
 
 export default nextConfig
